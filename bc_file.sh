1b9ab688da4554d2033eed6baccd6acd6d53de22
kwankhade1999 <kwankhade1999@gmail.com>
2025-10-05 03:39:55 +0530

    bash script

---
+++ bc_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_DIR="/home/kunal/Desktop/linux/shared"
+
+BACKUP_DIR="/home/kunal/Desktop/linux/backup"
+
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+
+BACKUP_FILE="backup_${DATE}.tar.gz"
+
+tar -czf "${BACKUP_DIR}/${BACKUP_FILE}" "${SOURCE_DIR}"
+
+if [ $? -eq 0 ]; then
+	 echo "✅ Backup successful: ${BACKUP_DIR}/${BACKUP_FILE}"
+else
+    echo "❌ Backup failed at $(date)"
+    exit 1
+fi
